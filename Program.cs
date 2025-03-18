86ac83301b8a4f0f8566c41ba4f5f6cf5962320a
MEGA <MEGA@gmail.com>
2025-03-18 18:02:44 +0200

    Pushing

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Stack
+            Console.WriteLine("palyndrome - 2ex");
+            Palyndrome();
+            
+        }
+        static void Palyndrome()
+        {
+            Queue<char> que = new();
+            string word = Console.ReadLine();
+            char[] charArr = word.ToCharArray();
+            foreach (char c in charArr)
+            {
+                que.Enqueue(c);
+            }
+            string rev = "";
+            char k;
+            while (que.Count > 0)
+            {
+                k = que.Dequeue();
+                rev += k;
+            }
+            if (rev.Equals(word))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
